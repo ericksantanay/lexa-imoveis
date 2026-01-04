f84597e21bee9e07d8899b9957ba31cccf9363b3
Erick Silva <donizttierick@gmail.com>
2026-01-04 15:10:37 -0300

    .

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+let menuB = document.getElementById('menuBurguer')
+
+function ClickMenu() {
+    if (menuMobile.style.display == 'block') {
+        menuMobile.style.display = 'none'
+    }else {
+        menuMobile.style.display = 'block'
+    }
+}
+
